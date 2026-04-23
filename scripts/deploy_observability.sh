c0295beb10677f782b3950026dd3a1b14a5373d4
cvgm <cvgmsys@gmail.com>
2026-04-23 13:33:19 -0400

    prometheus added

--- scripts/deploy_observability.sh
+++ scripts/deploy_observability.sh
@@ -8,19 +8,31 @@ kubectl apply -f https://github.com/cert-manager/cert-manager/releases/download/
 kubectl wait --for=condition=Available deployment/cert-manager-webhook -n cert-manager --timeout=120s
 sleep 30
 
-echo "2. Instalando el Operator de OpenTelemetry..."
+echo "2. Creando namespaces..."
+kubectl apply -f k8s/0-init/namespaces.yaml
+
+echo "3. Preparando repositorios de Helm..."
+helm repo add prometheus-community https://prometheus-community.github.io/helm-charts
+helm repo add opensearch https://opensearch-project.github.io/helm-charts/
+helm repo update
+
+# ==========================================
+# OPERATOR OPENTELEMETRY
+# ==========================================
+echo "4. Instalando el Operator de OpenTelemetry..."
 kubectl apply -f https://github.com/open-telemetry/opentelemetry-operator/releases/download/v0.148.0/opentelemetry-operator.yaml
 kubectl wait --for=condition=Available deployment/opentelemetry-operator-controller-manager -n opentelemetry-operator-system --timeout=120s
 
-echo "3. Creando el Namespace.."
-kubectl apply -f k8s/0-init/namespaces.yaml
+# ==========================================
+# PROMETHEUS
+# ==========================================
+echo "5. Desplegando Prometheus..."
+helm upgrade --install prometheus prometheus-community/prometheus --namespace prometheus --create-namespace -f k8s/1-observability/prometheus-values.yaml
 
 # ==========================================
 # OPENSEARCH
 # ==========================================
-echo "4. Desplegando OpenSearch.."
-helm repo add opensearch https://opensearch-project.github.io/helm-charts/
-helm repo update
+echo "6. Desplegando OpenSearch..."
 # sudo sysctl -w vm.max_map_count=262144
 helm upgrade --install opensearch opensearch/opensearch -f k8s/1-observability/opensearch-values.yaml --namespace observability
 kubectl wait --for=condition=Ready pod -l app.kubernetes.io/component=opensearch-cluster-master -n observability --timeout=300s
@@ -30,7 +42,7 @@ kubectl wait --for=condition=Ready pod -l app.kubernetes.io/component=opensearch
 # ==========================================
 # JAEGER
 # ==========================================
-echo "5. Desplegando Jaeger.."
+echo "7. Desplegando Jaeger..."
 kubectl apply -f k8s/1-observability/jaeger.yaml
 sleep 10
 kubectl wait --for=condition=Ready pod -l app.kubernetes.io/name=jaeger-backend-collector -n observability --timeout=300s
@@ -38,15 +50,16 @@ kubectl wait --for=condition=Ready pod -l app.kubernetes.io/name=jaeger-backend-
 # ==========================================
 # OPENTELEMETRY COLLECTOR
 # ==========================================
-echo "6. Desplegando Otel Collector.."
+echo "8. Desplegando Otel Collector..."
 kubectl apply -f k8s/1-observability/otel-collector.yaml
 
 # ==========================================
 # AUTO-INSTRUMENTATION
 # ==========================================
-echo "7. Configurando auto-instrumentación.."
+echo "9. Configurando auto-instrumentación..."
 kubectl apply -f k8s/1-observability/instrumentation.yaml
 
 
 echo "✅ Despliegue completado con éxito"
 echo "Jaeger UI: http://localhost:30686"
+echo "Prometheus UI: http://localhost:30090"
